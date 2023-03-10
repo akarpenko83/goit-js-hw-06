ac2662c38e6d842619b54cbee96c774dfd8c063c
Anton K <akarpenko83@gmail.com>
2023-03-10 14:28:41 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const btnRef = document.querySelector('button.change-color');
+const bodyRef = document.querySelector('body');
+const spanRef = document.querySelector('span.color');
+
+btnRef.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+  const hexColor = getRandomHexColor()
+  bodyRef.style.backgroundColor = `${hexColor}`;
+  spanRef.textContent = `${hexColor}`;
+ };
